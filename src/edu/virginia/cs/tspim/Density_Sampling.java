ba4f6df36fcd5e79c9764a19938602e1cdaf82ab
Tanmoy058 <31170646+Tanmoy058@users.noreply.github.com>
2017-10-24 17:12:49 -0400

    Add files via upload

--- src/edu/virginia/cs/tspim/Density_Sampling.java
+++ src/edu/virginia/cs/tspim/Density_Sampling.java
@@ -1,16 +1,45 @@
 package edu.virginia.cs.tspim;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.io.File;
 
-import javax.imageio.ImageIO;
-
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-
+import edu.virginia.cs.tspim.util.Util;
 
 public class Density_Sampling {
+	int [][]sampled_array;
+	int [][]presampled_array;
+	float threshold;
 	
+	Density_Sampling(int[][]mat, float avg)
+	{
+		threshold = avg;
+	    presampled_array = new int [mat.length][mat[0].length];
+		presampled_array = mat;
+		sampled_array = new int [mat.length-1][mat[0].length-1];
+		
+	}
+	
+	void gen_Sample()
+	{
+		
+		System.out.println(presampled_array.length + " "+presampled_array[0].length);
+		//int k=0,l=0;
+		for(int i=0,k=0;i<presampled_array.length-1;i++,k++)
+		{
+			
+			for(int j=0,l=0;j<presampled_array[0].length-1;j++,l++)
+			{
+				sampled_array[k][l]=(presampled_array[i][j]+presampled_array[i+1][j]+presampled_array[i][j+1]+presampled_array[i+1][j+1])/4;
+			}
+		}
+		
+		/*for(int i=0;i<sampled_array.length;i++)
+		{
+			for(int j=0;j<sampled_array[0].length;j++)
+			{
+				System.out.print(sampled_array[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println(sampled_array.length + " "+sampled_array[0].length);*/
+		
+	}
 
 }
--- src/edu/virginia/cs/tspim/GrayScale_Conversion.java
+++ src/edu/virginia/cs/tspim/GrayScale_Conversion.java
@@ -12,11 +12,22 @@ import org.opencv.imgproc.Imgproc;
 class GrayScale_Conversion {
 	
 	String image_file;
+	int sum;
+	float average_threshold;
+	int [][]sample_pre_array;
 	
 	GrayScale_Conversion(String img_file)
 	{
 		image_file = img_file;
+		sum =0;
+		average_threshold =0;
 	}
+	
+	public float get_threshold()
+	{
+		return average_threshold;
+	}
+	
 	
 	public void convert_to_Gray(){
 	try {
@@ -27,22 +38,61 @@ class GrayScale_Conversion {
         byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
         Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
         mat.put(0, 0, data);
-
+                
         Mat mat1 = new Mat(image.getHeight(),image.getWidth(),CvType.CV_8UC1);
         Imgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2GRAY);
-       
+        //Imgproc.threshold(mat,mat1, 127,255, Imgproc.THRESH_BINARY);
 
         byte[] data1 = new byte[mat1.rows() * mat1.cols() * (int)(mat1.elemSize())];
         mat1.get(0, 0, data1);
+         
         BufferedImage image1 = new BufferedImage(mat1.cols(),mat1.rows(), BufferedImage.TYPE_BYTE_GRAY);
         image1.getRaster().setDataElements(0, 0, mat1.cols(), mat1.rows(), data1);
-
-        File ouptut = new File("grayscale.jpg");
-        ImageIO.write(image1, "jpg", ouptut);
         
+     
+       Mat mat2 = new Mat(image1.getHeight(),image1.getWidth(),CvType.CV_8UC1);
+       //Imgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2GRAY);
+       Imgproc.threshold(mat1,mat2, 127,255, Imgproc.THRESH_BINARY);
+
+       byte[] data2 = new byte[mat2.rows() * mat2.cols() * (int)(mat1.elemSize())];
+       mat2.get(0, 0, data2);
+       sample_pre_array = new int [mat2.rows()][mat2.cols()];
        
+       for(int i=0;i<mat2.rows();i++)
+       {
+       	for(int j=0;j<mat2.cols();j++)
+       	{
+       		int a = data2[i+j] & 0xff;
+       		//System.out.print(a+" "); 
+       		sample_pre_array[i][j]=a;
+       	}
+       	//System.out.println();
+       }
+       
+       for(int i=0;i<mat2.rows();i++)
+       {
+       	for(int j=0;j<mat2.cols();j++)
+       	{
+       		sum+=sample_pre_array[i][j]; 
+       	}
+       }
+      
+       average_threshold = sum/4;
+       
+       Density_Sampling s = new Density_Sampling(sample_pre_array,average_threshold);
+       s.gen_Sample();
+       
+       File ouptut = new File("grayscale.jpg");
+       ImageIO.write(image1, "jpg", ouptut);
+        
+           
      } catch (Exception e) {
         System.out.println("Error: " + e.getMessage());
      }
+	}
+
+	public int[][] get_array() {
+		
+		return sample_pre_array;
 	}
 }
